101eadb67e65d34e37aff41c59afe35e162c3f55
Hyeongyu-lee <A2020011@kookmin.ac.kr>
2020-10-05 02:24:58 -0400

    update_10051524

--- src/lane_detect.cpp
+++ src/lane_detect.cpp
@@ -613,11 +613,12 @@ LaneDetector::LaneDetector(ros::NodeHandle nh)
 
 	int LaneDetector::display_img(Mat _frame, int _delay, bool _view) {
 		Mat new_frame, warped_frame, gray_frame, blur_frame, binary_frame, sliding_frame, resized_frame;
+		Mat filter(5, 5, CV_8U, Scalar(1));
 
 		resize(_frame, new_frame, Size(width_, height_));
 		warped_frame = warped_img(new_frame);
 		cvtColor(warped_frame, gray_frame, COLOR_BGR2GRAY);
-		medianBlur(gray_frame, blur_frame, 5);
+		morphologyEx(gray_frame, blur_frame, MORPH_OPEN, filter);
 		threshold(blur_frame, binary_frame, 175, 255, THRESH_BINARY);
 		//binary_frame = pipeline_img(blur_frame);
 		sliding_frame = detect_lines_sliding_window(binary_frame, _view);
